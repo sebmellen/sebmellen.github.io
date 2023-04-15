843f793b1fe335079c5c3608a26ac43ac8f01117
Sebastian Mellen <sebastian@cerebrum.com>
2023-04-15 04:06:42 -0400

    Try to fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   purge: ["./**/*.html", "./**/*.js"],
   darkMode: false,
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-      backgroundClip: ["hover", "focus"],
-    },
-  },
   plugins: [],
 };
