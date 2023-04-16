3b6214b6ef4fee912cf8e1e9224fe491a418fd30
Sebastian Mellen <sebastian@cerebrum.com>
2023-04-16 03:50:37 -0400

    fix gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,4 @@ module.exports = {
   mode: "jit",
   content: ["index.html", "./**/*.js"],
   plugins: [],
-  theme: {
-    extend: {
-      backgroundImage: {
-        main: "url('/assets/bg.png')",
-      },
-    },
-  },
 };
